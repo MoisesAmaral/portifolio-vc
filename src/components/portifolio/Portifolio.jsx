6259685ca9edfd8824ba103e3d8f25f0261b33db
MoisesDev <moises.amaraladm@gmail.com>
2022-03-13 11:05:51 -0300

    Segunda parte do projeto comitada

---
+++ src/components/portifolio/Portifolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './portifolio.css'
+
+const Portifolio = () => {
+  return (
+    <section id='portifolio'>Portifolio</section>
+    )
+}
+export default Portifolio
