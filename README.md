cd811e091b8962ef0e8d68a8462bff445e4f3c09
MoisesDev <moises.amaraladm@gmail.com>
2022-03-15 19:39:38 -0300

    terceira parte do projeto concluida

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto portifolio com React Js
+
+- Listo abaixo alguma Ferramentas usadas para o desenvlvimento deste projeto
+
+- emailjs-com > Ferramenta para envio de emails, mais informações podem ser     encontradas no site da ferramenta: https://www.emailjs.com/
+    
+- react-icons > ferramenta para uso de icones personalizado dentro do React.
+
+- swiper > Ferramenta para criação de slids, ideal para slids de testemunhos etc.. mais informações sobre esta ferramenta: https://swiperjs.com/
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ygwz8rj', 'template_7ydx43d', form.current, 'T8vahxIa8t9uqIx90')
+     
+      e.target.reset()
+  };
+
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>moises.amaraladm@gmail.com</h5>
+            <a href="mailto:moises.amaraladm@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messeger</h4>
+            <h5>MoisesDev- developer</h5>
+            <a href="https://m.me/sistemasblu"target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+5531998268940</h5>
+            <a href="https://api.whatsapp.com/send?phone=+5531998268940"target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/*END CONTACT OPTIONS*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='You Full Name' required />
+          <input type="email" name='email' placeholder='Your Email'required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
